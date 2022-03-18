f6c21c3092e73eda0029973b81d978f29e754c0d
shaimaa1236 <60436597+shaimaa1236@users.noreply.github.com>
2022-03-18 23:16:35 +0300

    .

---
+++ day6-cw3/day6-cw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day6-cw3
+//
+//  Created by Shaimaa on 18/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day6-cw3/day6-cw3/day6_cw3App.swift
@@ -0,0 +1,17 @@
+//
+//  day6_cw3App.swift
+//  day6-cw3
+//
+//  Created by Shaimaa on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct day6_cw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
